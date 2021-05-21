9ca90139cade494e1acf6be24953d9ec0a6ca4fa
Ben <bendelathouwer@users.noreply.github.com>
2021-05-21 13:04:58 +0100

    klaar
    pcb klaar

--- software/arduino test code/rtc test code/rtc_test_code/rtc_test_code.ino
+++ software/arduino test code/rtc test code/rtc_test_code/rtc_test_code.ino
@@ -1,20 +1,33 @@
 #include <RTCZero.h>
 
 RTCZero rtc;
-const byte minutes = 0;
+const byte seconds = 0;
+const byte minutes = 00;
+const byte hours = 00;
 
+int value = 0;
 void setup()
 {
   Serial.begin(9600);
   rtc.begin(); // initialize RTC
+  rtc.setAlarmSeconds(240);
+  rtc.enableAlarm(rtc.MATCH_SS);
+  rtc.attachInterrupt(alarmMatch);
+
+  Serial.println("start ");
 }
 
 void loop()
 {
-  int timer = rtc.getMinutes() % 2;
-  if (timer == 1)
+
+  if (value == 1)
   {
-    Serial.println("2 minuuten gepaseerd ");
+    Serial.println("2 minuten gedaan ");
+    value = 0;
   }
+}
+void alarmMatch()
+{
+  value = 1;
 
 }
